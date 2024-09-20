5ce9852c9dba3cc1319f2196a6c1f5a06057c6e7
Orlandoprieto <orlandoprieto19@gmail.com>
2024-09-20 12:48:57 -0300

    Se agrego botones para poder editar posts del usuario registrado

--- src/components/cardPosts/CardPost.tsx
+++ src/components/cardPosts/CardPost.tsx
@@ -5,7 +5,10 @@ import './cardPostStyles.css'
 import iconAdmin from '../../assets/iconAdmin.png'
 // @ts-ignore
 import iconEdit from '../../assets/icon_edit.png'
+
 import { User } from "../../validations/user"
+import { ButtonSecondary } from "../button/Button"
+import { recoverLogin } from "../../config/utils"
 
 interface CardProps {
     post: Post;
@@ -13,15 +16,25 @@ interface CardProps {
 }
 
 export default function CardPost({ post, user }: CardProps) {
+    const sessionUser = recoverLogin()
+
     const viewPost = () => {
-        
+
     }
 
     return (
         <div className="cardPost" onClick={viewPost}>
-            <div className="user_post">
-                <img className="avatar_user" src={iconAdmin}/>
-                <span>{user?.name}</span>
+            <div>
+                <div className="user_post">
+                    <img className="avatar_user" src={iconAdmin} />
+                    <span>{user?.name}</span>
+                </div>
+
+                {post.userId == sessionUser?.id && (
+                    <div>
+                        <ButtonSecondary icon={iconEdit} />
+                    </div>
+                )}
             </div>
 
             <div className="title_post">
@@ -31,7 +44,6 @@ export default function CardPost({ post, user }: CardProps) {
             <div className="body_post">
                 <span>{post.body}</span>
             </div>
-
         </div>
     )
 }
